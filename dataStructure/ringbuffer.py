73a94eb0570efce74a372b798946c3ac0de84254
sangmoon <cholsoo22001@gmail.com>
2019-12-20 00:00:14 +0900

    python ringbuffer 템플릿 추가

---
+++ dataStructure/ringbuffer.py
@@ -0,0 +1,33 @@
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None for i in range(size)]
+        self.front = self.rear = -1
+    
+    def enqueue(self, data):
+        """
+        add an element into the queue
+        """
+        
+        # if queue if full
+
+        # if queue is empty
+
+        # else
+        pass
+
+    def dequeue(self):
+        """
+        get an element from the queue
+        """
+        # if queue is empty
+
+        # if queue has only 1
+
+        # else
+
+    def show(self):
+        """
+        display elements in the queue
+        """
+        pass
