a8cac8ece57c2f93964735813bb7bc3253c81791
sangmoon <cholsoo22001@gmail.com>
2019-12-22 23:38:58 +0900

    stack queue 추가

---
+++ python/dataStructure/stack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
